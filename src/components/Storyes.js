0ac2f71603b983a5f2c7a59eacc551d5cd4807a6
nicolas00000 <nicmariano10@gmail.com>
2023-07-04 10:25:25 -0300

    add image

--- src/components/Storyes.js
+++ src/components/Storyes.js
@@ -1,29 +1,27 @@
 export default function Storyes(){
-
 let nome = 
 [
-    {src: "9gag.svg", name: "9gag"}, 
-    {src: "meowed.svg", name: "meowed"},
-    {src: "barked.svg", name: "barked"},
-    {src: "nathanwpylestrangeplanet.svg", name: "nathanwpylestrangeplanet"},
-    {src: "wawawicomics.svg", name: "wawawicomics"},
-    {src: "respondeai.svg", name: "respondeai"},
-    {src: "filomoderna.svg", name: "filomoderna"},
-    {src: "memeriagourmet.svg", name: "memeriagourmet"},
+    {src: "assets/img/gag.svg", name: "gag"}, 
+    {src: "assets/img/meowed.svg", name: "meowed"},
+    {src: "assets/img/barked.svg", name: "barked"},
+    {src: "assets/img/nathanwpylestrangeplanet.svg", name: "nathanwpylestrangeplanet"},
+    {src: "assets/img/wawawicomics.svg", name: "wawawicomics"},
+    {src: "assets/img/respondeai.svg", name: "respondeai"},
+    {src: "assets/img/filomoderna.svg", name: "filomoderna"},
+    {src: "assets/img/memeriagourmet.svg", name: "memeriagourmet"},
 ] 
-
     return(
         <div class="stories">
-            {nome.map(nome => 
-                <div class="story">
-                <div class="imagem">
-                    <img src="assets/img/memeriagourmet.svg" />
+            {nome.map((nome, index) => (
+                <div class="story" key={index}>
+                    <div class="imagem">
+                        <img src={nome.src} />
+                    </div>
+                    <div class="usuario">
+                        {nome.name}
+                    </div>
                 </div>
-                <div class="usuario">
-                    {nome.name}
-                </div>
-            </div>
-            )}
+        ))}
         <div class="setinha">
             <ion-icon name="chevron-forward-circle"></ion-icon>
         </div>
